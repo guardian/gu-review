f7a0f3235e5d6af4efc1c9bbcbb6891d443817f5
Robert Berry <robert.berry@theguardian.com>
2014-07-14 17:12:22 +0100

    Hook up persistence

---
+++ app/data/Persistence.scala
@@ -0,0 +1,12 @@
+package data
+
+import com.amazonaws.regions.{Region, Regions}
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsyncClient
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object Persistence {
+  private val dynamoDbClient = new AmazonDynamoDBAsyncClient()
+  dynamoDbClient.setRegion(new Region(Regions.EU_WEST_1))
+
+  val reviews = ReviewsTable(dynamoDbClient, "guReviews")
+}
