05b1628ecb1f18370dc160cacdf3219739afdd91
Robert Berry <robert.berry@theguardian.com>
2014-07-15 12:58:19 +0100

    Display username for name

--- app/controllers/Helpers.scala
+++ app/controllers/Helpers.scala
@@ -4,18 +4,41 @@ import data.{Statistics, Persistence}
 import model._
 import org.joda.time.DateTime
 import play.api.mvc.{Action, Controller}
+import Persistence.reviews.record
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 object Helpers extends Controller {
   def insertFixture() = Action.async {
-    Persistence.reviews.record(Review(
-      ContentId("books/2014/jul/13/empty-mansions-review-bill-dedman-huguette-clark"),
-      UserId("123456"),
-      model.Indifferent,
-      Some(Comment("Comment")),
-      DateTime.now,
-      rating = 1
+    val contentId = ContentId("books/2014/jul/13/empty-mansions-review-bill-dedman-huguette-clark")
+
+    Future.sequence(Seq(
+      record(Review(
+        contentId,
+        UserId("adamnfish"),
+        LovedIt,
+        Some(Comment("This was wonderful. It brought tears to my eyes. It made me rue the day I decided to become " +
+          "a computer programmer - if only I could be a reclusive heiress!")),
+        DateTime.now,
+        rating = -12
+      )),
+      record(Review(
+        contentId,
+        UserId("robertchristgau"),
+        HatedIt,
+        Some(Comment("Ersatz shit")),
+        DateTime.now.minusHours(2).minusMinutes(20),
+        rating = 20
+      )),
+      record(Review(
+        contentId,
+        UserId("franciscarr"),
+        Indifferent,
+        None,
+        DateTime.now.minusHours(1).minusMinutes(3).minusSeconds(45),
+        rating = 0
+      ))
     )).map(Function.const(Ok("Inserted")))
   }
 
