c7e34a26536b3f7c623822648569f6806485dbbb
adamnfish <adamnfish@gmail.com>
2014-07-15 11:14:10 +0100

    Add face images and tweak JS

--- app/views/assets/init.scala.js
+++ app/views/assets/init.scala.js
@@ -6,19 +6,22 @@ require(["bonzo", "qwery", "bean", "reqwest"], function(bonzo, qwery, bean, reqw
         },
         isReviewPage = function() {
             return guardian.config.page.tones == "Reviews" && guardian.config.page.section == "books";
-        };
+        },
         initialLoad = function() {
             if (isReviewPage()) {
-                var reviewDataUrl = "@domain/reviews/" + document.location.pathname;
+                var reviewDataUrl = "@domain/reviews" + document.location.pathname + "?callback=?";
                 reqwest({
                     url: reviewDataUrl,
                     method: 'get',
+                    type: "jsonp",
                     success: function(response) {
-                        $(".discussion__comments__container").html(response);
+                        // insert stats and vote controls
+                        $(".discussion__comments__container").html(response.html);
                     }
                 });
             }
         };
 
+    console.log("blah", isReviewPage());
     initialLoad();
 });
--- app/views/assets/update.scala.js
+++ app/views/assets/update.scala.js
@@ -1,1 +1,28 @@
 @(domain: JavaScript)
+
+require(["bonzo", "qwery", "bean", "reqwest"], function(bonzo, qwery, bean, reqwest){
+    var doReview = function(sentiment, comment){
+        var reviewDataUrl = "@domain/reviews" + document.location.pathname + "?callback=?",
+            data;
+        if (typeof comment === 'undefined') {
+            data = {
+                sentiment: sentiment
+            };
+        } else {
+            data = {
+                sentiment: sentiment,
+                comment: comment
+            };
+        }
+
+        reqwest({
+            url: reviewDataUrl,
+            method: 'post',
+            data: data,
+            success: function(response) {
+                // insert stats and vote controls
+                $(".discussion__comments__container").html(response.html);
+            }
+        });
+    }
+});
