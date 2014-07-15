0dd4e97f01c1470221131821b8f9a2f4bfc142b7
Robert Berry <robert.berry@theguardian.com>
2014-07-15 13:08:52 +0100

    Add CORS

---
+++ app/filters/CorsFilter.scala
@@ -0,0 +1,15 @@
+package filters
+
+import play.api.mvc.{Filter, RequestHeader, Result}
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object CorsFilter extends Filter {
+  override def apply(f: (RequestHeader) => Future[Result])(rh: RequestHeader): Future[Result] = {
+    f(rh).map(_.withHeaders( "Access-Control-Allow-Origin" -> "*",
+      "Access-Control-Allow-Methods" -> "POST, GET, OPTIONS, PUT, DELETE",
+      "Access-Control-Allow-Headers" -> "x-requested-with,content-type,Cache-Control,Pragma,Date"
+    ))
+  }
+}
